05f03ae36273d1f82d7dc9716bbbb42c05fed6e3
Julien Fereira <ferreira-julien@live.fr>
2022-11-16 23:38:42 +0100

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+  # resources :restaurants do
+  # ressources :reviews, only:
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
